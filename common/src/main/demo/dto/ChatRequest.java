54bd3b8c162c38fff7c2d87a07d3e75c701982d9
Rahul <rahul.kumar@nitsolution.in>
2024-01-29 19:35:40 +0530

    trying to intergrate with chat gpt

---
+++ common/src/main/demo/dto/ChatRequest.java
@@ -0,0 +1,31 @@
+package demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRequest {
+
+    private String model;
+    private List<Message> messages;
+    private int n;
+    private double temperature;
+
+    public ChatRequest(String model, String prompt) {
+        this.model = model;
+
+        this.messages = new ArrayList<>();
+        this.messages.add(new Message("user", prompt));
+    }
+
+    // getters and setters
+}
+
